81cb43574b0c23584f65d1d7dadff1bc1b327dba
Cristian Flores Martínez <acrixf@gmail.com>
2020-12-26 22:02:58 -0600

    Code Base Persistence Best Practices

---
+++ src/main/java/org/neoa/SpringPersistenceApplication.java
@@ -0,0 +1,15 @@
+package org.neoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringPersistenceApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPersistenceApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/neoa/entity/Book.java
@@ -0,0 +1,46 @@
+package org.neoa.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+public class Book implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String isbn;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id")
+    private Author author;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (this == obj) {
+            return true;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        return id != null && id.equals(((Book) obj).id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 2021;
+    }
+    @Override
+    public String toString() {
+        return "Book{" + "id=" + id + ", title=" + title
+                + ", isbn=" + isbn + '}';
+    }
+}
