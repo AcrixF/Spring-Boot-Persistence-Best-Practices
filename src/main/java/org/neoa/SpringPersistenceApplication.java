7efbbad3628db6fa5b26947e1777e35c122ac9a4
Cristian Flores Martínez <acrixf@gmail.com>
2020-12-26 23:50:46 -0600

    invoke insert method in application bootstraping

--- src/main/java/org/neoa/SpringPersistenceApplication.java
+++ src/main/java/org/neoa/SpringPersistenceApplication.java
@@ -1,9 +1,6 @@
 package org.neoa;
 
-import org.neoa.entity.Author;
-import org.neoa.entity.Book;
-import org.neoa.repository.AuthorRepository;
-import org.neoa.repository.BookRepository;
+import org.neoa.service.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -12,14 +9,11 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringPersistenceApplication {
-
-    private AuthorRepository authorRepository;
-    private BookRepository bookRepository;
+    private final AuthorService authorService;
 
     @Autowired
-    public SpringPersistenceApplication(AuthorRepository authorRepository, BookRepository bookRepository) {
-        this.authorRepository = authorRepository;
-        this.bookRepository = bookRepository;
+    public SpringPersistenceApplication(AuthorService authorService) {
+        this.authorService = authorService;
     }
 
     public static void main(String[] args) {
@@ -28,20 +22,9 @@ public class SpringPersistenceApplication {
 
     @Bean
     public CommandLineRunner commandLineRunner() {
-        return (args) -> {
-            Author author = new Author()
-                    .setName("Martin Fowler")
-                    .setAge(50)
-                    .setGenre("Programming");
+        return args ->
+            authorService.insertAuthorWithBooks();
 
-            Book book = new Book()
-                    .setIsbn("978-0-13-475759-9")
-                    .setAuthor(author)
-                    .setTitle("Refactoring Improving the Design of Existing Code");
-
-            author.addBook(book);
-
-            authorRepository.saveAndFlush(author);
-        };
     }
+
 }
