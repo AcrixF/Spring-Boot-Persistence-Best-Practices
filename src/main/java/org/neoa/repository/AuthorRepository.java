59d548f3f3e837d2af9719f58d5c9334feaf18e3
Cristian Flores Martínez <acrixf@gmail.com>
2020-12-26 23:48:08 -0600

    refactor Entity type in repository definition

--- src/main/java/org/neoa/repository/AuthorRepository.java
+++ src/main/java/org/neoa/repository/AuthorRepository.java
@@ -1,7 +1,7 @@
 package org.neoa.repository;
 
-import org.neoa.entity.Author;
+import org.neoa.entity.AuthorEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AuthorRepository extends JpaRepository<Author, Long> {
+public interface AuthorRepository extends JpaRepository<AuthorEntity, Long> {
 }
