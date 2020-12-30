6498e5b621b361bf660702aaa5c111dad7356d98
Cristian Flores <cristian.flores@deepsee.ai>
2020-12-30 17:12:07 -0600

    Cleaning Repository

--- src/main/java/org/neoa/repository/AuthorRepository.java
+++ src/main/java/org/neoa/repository/AuthorRepository.java
@@ -3,8 +3,6 @@ package org.neoa.repository;
 import org.neoa.entity.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface AuthorRepository extends JpaRepository<Author, Long> {
-    Optional<Author> findAuthorEntityByName(String name);
+
 }
