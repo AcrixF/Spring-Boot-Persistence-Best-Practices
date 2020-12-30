8d637244fa23d1a78b714d559c54b1d50ba51651
Cristian Flores <cristian.flores@deepsee.ai>
2020-12-30 16:34:07 -0600

    cleaning up service

--- src/main/java/org/neoa/repository/BookRepository.java
+++ src/main/java/org/neoa/repository/BookRepository.java
@@ -1,23 +1,9 @@
 package org.neoa.repository;
 
 import org.neoa.entity.Book;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
-
-    @Transactional
-    @Query("SELECT b FROM Book b WHERE b.author.id = :id")
-    List<Book> fetchBooksOfAuthorById(Long id);
-
-    @Transactional(readOnly = true)
-    @Query("select b from Book b where b.author.id = :id")
-    Page<Book> fetchPageBooksOfAuthorById(Long id, Pageable pageable);
 }
