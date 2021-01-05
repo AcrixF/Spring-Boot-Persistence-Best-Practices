ebd31e4b4d3c9947bbddf230dcfd40b82b45c133
Cristian Flores Martínez <acrixf@gmail.com>
2021-01-05 15:45:42 -0600

    Defining Specification for Aged

---
+++ src/main/java/org/neoa/repository/specifications/AuthorSpecs.java
@@ -0,0 +1,17 @@
+package org.neoa.repository.specifications;
+
+import org.neoa.entity.Author;
+import org.springframework.data.jpa.domain.Specification;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+public class AuthorSpecs {
+    private static final int AGE = 45;
+
+    public static Specification<Author> isAgeGt45() {
+        return (Root<Author> root, CriteriaQuery<?> query, CriteriaBuilder builder)
+                ->  builder.greaterThan(root.get("age"), AGE);
+    }
+}
