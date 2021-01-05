5f8ba25e74f2c017865033c8391ce60e30a0e9f8
Cristian Flores Martínez <acrixf@gmail.com>
2021-01-05 15:46:27 -0600

    Extending JpaSpecification in the Author Repository

--- src/main/java/org/neoa/repository/AuthorRepository.java
+++ src/main/java/org/neoa/repository/AuthorRepository.java
@@ -1,8 +1,10 @@
 package org.neoa.repository;
 
 import org.neoa.entity.Author;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,14 +12,14 @@ import java.util.List;
 
 @Repository
 @Transactional(readOnly = true)
-public interface AuthorRepository extends JpaRepository<Author, Long> {
+public interface AuthorRepository extends JpaRepository<Author, Long>, JpaSpecificationExecutor<Author> {
 
     @Override
     @EntityGraph(
             value = "author-books-graph",
             type = EntityGraph.EntityGraphType.FETCH
     )
-    List<Author> findAll();
+    List<Author> findAll(Specification specification);
 
     @EntityGraph(
             value = "author-books-graph",
