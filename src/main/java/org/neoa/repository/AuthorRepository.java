fd7f15935bbb66e121588a535448a60e746bfb21
Cristian Flores Martínez <acrixf@gmail.com>
2021-01-05 12:16:40 -0600

    add a new Query Method using the Query Builder Mechanism

--- src/main/java/org/neoa/repository/AuthorRepository.java
+++ src/main/java/org/neoa/repository/AuthorRepository.java
@@ -1,11 +1,27 @@
 package org.neoa.repository;
 
 import org.neoa.entity.Author;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Repository
 @Transactional(readOnly = true)
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+
+    @Override
+    @EntityGraph(
+            value = "author-books-graph",
+            type = EntityGraph.EntityGraphType.FETCH
+    )
+    List<Author> findAll();
+
+    @EntityGraph(
+            value = "author-books-graph",
+            type = EntityGraph.EntityGraphType.FETCH
+    )
+    List<Author> findByAgeLessThanOrderByNameDesc(int age);
 }
