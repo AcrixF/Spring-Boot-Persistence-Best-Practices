5a43c42dd624a83567e16070131317df9434d8cc
Cristian Flores Martínez <acrixf@gmail.com>
2021-01-05 16:16:05 -0600

    Create the JPQL Query in the Author Repository

--- src/main/java/org/neoa/repository/AuthorRepository.java
+++ src/main/java/org/neoa/repository/AuthorRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,4 +27,11 @@ public interface AuthorRepository extends JpaRepository<Author, Long>, JpaSpecif
             type = EntityGraph.EntityGraphType.FETCH
     )
     List<Author> findByAgeLessThanOrderByNameDesc(int age);
+
+    @EntityGraph(
+            value = "author-books-graph",
+            type = EntityGraph.EntityGraphType.FETCH
+    )
+    @Query("select a from Author a where a.age > 20 and a.age < 40")
+    List<Author> fetchAllAgeBetween20And40();
 }
