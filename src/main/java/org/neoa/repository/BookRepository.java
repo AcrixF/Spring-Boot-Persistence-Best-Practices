1bce70c333f540f5c2d2d823e7876d2573fb3e4e
Cristian Flores Martínez <acrixf@gmail.com>
2021-01-10 22:42:06 -0600

    clean up

--- src/main/java/org/neoa/repository/BookRepository.java
+++ src/main/java/org/neoa/repository/BookRepository.java
@@ -1,14 +1,8 @@
 package org.neoa.repository;
 
-import org.neoa.entity.Author;
 import org.neoa.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
