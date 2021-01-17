c58977b9df9f9077222dac1fb1e4929d417e0075
Cristian Flores Martínez <acrixf@gmail.com>
2021-01-17 17:11:51 -0600

    clean up

--- src/main/java/org/neoa/service/AuthorService.java
+++ src/main/java/org/neoa/service/AuthorService.java
@@ -1,7 +1,6 @@
 package org.neoa.service;
 
 import lombok.extern.log4j.Log4j2;
-import org.neoa.entity.Author;
 import org.neoa.repository.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,12 +20,6 @@ public class AuthorService {
     @Transactional
     public void insertAuthors() {
         log.info("Inserting Author");
-        Author martin = new Author()
-                .setName("Martin")
-                .setGenre("Tech")
-                .setAge(55);
-
-        authorRepository.saveAndFlush(martin);
     }
 
 
