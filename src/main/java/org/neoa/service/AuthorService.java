11163df6f0a001b2112a55a0bce7df229ad43bf6
Cristian Flores Martínez <acrixf@gmail.com>
2021-01-18 19:01:21 -0600

    Clean up Services

--- src/main/java/org/neoa/service/AuthorService.java
+++ src/main/java/org/neoa/service/AuthorService.java
@@ -1,11 +1,9 @@
 package org.neoa.service;
 
 import lombok.extern.log4j.Log4j2;
-import org.neoa.entity.Author;
 import org.neoa.repository.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Log4j2
@@ -18,37 +16,6 @@ public class AuthorService {
         this.authorRepository = repository;
     }
 
-    @Transactional
-    public void insertAuthor() {
-        log.info("Inserting Author");
-
-        Author author = new Author();
-        author.setName("Joana Nimar");
-        author.setGenre("History");
-        author.setAge(34);
-        authorRepository.save(author);
-    }
-
-    @Transactional(readOnly = true)
-    public void fetchAuthor() {
-        log.info("Fetching Author From Second Cache Level");
-        Author author = authorRepository.findById(1L)
-                .orElseThrow();
-        System.out.println(author);
-    }
-
-    @Transactional
-    public void updateAuthor() {
-        log.debug("Updating Immutable Author");
-        Author author = authorRepository.findById(1L)
-                .orElseThrow();
-        author.setAge(45);
-    }
-
-    public void deleteAuthor() {
-        log.info("Deleting Immutable Author");
-        authorRepository.deleteById(1L);
-    }
 }
 
 
--- src/main/java/org/neoa/service/BookService.java
+++ src/main/java/org/neoa/service/BookService.java
@@ -1,51 +1,10 @@
 package org.neoa.service;
 
 import lombok.extern.log4j.Log4j2;
-import org.neoa.entity.Author;
-import org.neoa.entity.Book;
-import org.neoa.repository.AuthorRepository;
-import org.neoa.repository.BookRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Log4j2
 public class BookService {
-    private final BookRepository bookRepository;
-    private final AuthorRepository authorRepository;
-
-    @Autowired
-    public BookService(BookRepository bookRepository, AuthorRepository authorRepository) {
-        this.bookRepository = bookRepository;
-        this.authorRepository = authorRepository;
-    }
-
-    @Transactional
-    public void addBookToAuthorUsingFindBy() {
-        log.info("Adding book to Author using findById");
-        Author author = authorRepository.findById(1L).orElseThrow();
-
-        Book book = new Book();
-        book.setIsbn("001-MJ");
-        book.setTitle("The Canterbury Anthology");
-        book.setAuthor(author);
-
-        bookRepository.save(book);
-    }
-
-    @Transactional
-    public void addBookToAuthorUsingGetOne() {
-        log.info("Adding book to Author using getOne");
-        Author proxy = authorRepository.getOne(1L);
-
-        Book book = new Book();
-        book.setIsbn("001-MJ");
-        book.setTitle("The Canterbury Anthology");
-        book.setAuthor(proxy);
-
-        bookRepository.save(book);
-    }
-
 
 }
