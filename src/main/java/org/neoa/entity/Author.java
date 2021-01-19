d2def26463e5af0210171d6564e3c30b1330aa8b
Cristian Flores Martínez <acrixf@gmail.com>
2021-01-18 18:12:06 -0600

    Add @Immutable and Cache Configuration to Author Entity

--- src/main/java/org/neoa/entity/Author.java
+++ src/main/java/org/neoa/entity/Author.java
@@ -3,22 +3,22 @@ package org.neoa.entity;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.Immutable;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @Setter
-@Entity
 @Accessors(chain = true)
+@Entity
+@Immutable
+@Cache(usage = CacheConcurrencyStrategy.READ_ONLY, region = "Author")
 public class Author implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,25 +26,6 @@ public class Author implements Serializable {
     private String name;
     private String genre;
     private int age;
-
-    @OneToMany(
-            cascade = CascadeType.ALL,
-            orphanRemoval = true,
-            fetch = FetchType.LAZY)
-    private List<Book> books = new ArrayList<>();
-
-
-    public Author addBook(Book book) {
-        book.setAuthor(this);
-        this.books.add(book);
-        return this;
-    }
-
-    public Author removeBook(Book book) {
-        book.setAuthor(null);
-        this.books.remove(book);
-        return this;
-    }
 
 
     @Override
